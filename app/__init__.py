9e5af7de183ddc309b14a2a331a8e1b851e09c86
luo.reid <luo.reid@gmail.com>
2021-02-16 23:02:07 +0800

    feat: auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
@@ -10,16 +10,21 @@ mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
-def create_app(config_name):
-  app = Flask(__name__)
-  app.config.from_object(config[config_name])
-  config[config_name].init_app(app)
-  bootstrap.init_app(app)
-  mail.init_app(app)
-  moment.init_app(app)
-  db.init_app(app)
 
-  #attach router and error page
-  from .main import main as main_blueprint
-  app.register_blueprint(main_blueprint)
-  return app
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    # attach router and error page
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    return app
